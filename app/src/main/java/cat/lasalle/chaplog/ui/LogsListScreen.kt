2cde9f751ca5e5dc39876f19ae6823a1501ec76b
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2024-01-10 11:03:17 +0100

    feat: show logs properly and delete dialog

--- app/src/main/java/cat/lasalle/chaplog/ui/LogsListScreen.kt
+++ app/src/main/java/cat/lasalle/chaplog/ui/LogsListScreen.kt
@@ -1,34 +1,108 @@
 package cat.lasalle.chaplog.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import cat.lasalle.chaplog.data.LogUiState
 import cat.lasalle.chaplog.data.LogsUiState
+import cat.lasalle.chaplog.model.LogsViewModel
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun LogsListScreen(
-    logsUiState: LogsUiState,
+    viewModel: LogsViewModel = LogsViewModel(),
 
     onLogClicked: (LogUiState) -> Unit,
-    onLogLongClicked: (LogUiState) -> Unit
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+    val openDeleteDialog = remember { mutableStateOf(false) }
+
     Column {
-        logsUiState.logs.forEach { log ->
+        uiState.logs.forEach { log ->
             ListItem(
-                headlineContent = { Text(text = "${log.title} by ${log.author}") },
-                supportingContent = { Text(text = "${log.currentPage} / ${log.pages}") },
+                modifier = Modifier.combinedClickable(
+                    onClick = { onLogClicked(log) },
+                    onLongClick = { openDeleteDialog.value = true }
+                ),
+
+                headlineContent = { Text(text = log.title) },
+                supportingContent = { Text(text = "${log.author} · ${log.currentPage} / ${log.pages}") },
                 trailingContent = {
                     Icon(
-                        imageVector = Icons.Filled.Edit,
-                        contentDescription = "Edit log"
+                        imageVector = Icons.Filled.KeyboardArrowRight,
+                        contentDescription = "Edit log",
                     )
                 },
             )
+            Divider()
+
+            when {
+                openDeleteDialog.value -> {
+                    LogDeleteDialog(
+                        log = log,
+                        onConfirmation = {
+                            viewModel.removeLog(log)
+                            openDeleteDialog.value = false
+                        },
+                        onDismissRequest = {
+                            openDeleteDialog.value = false
+                        }
+                    )
+                }
+            }
         }
     }
+}
+
+@Composable
+fun LogDeleteDialog(
+    log: LogUiState,
+    onConfirmation: () -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    AlertDialog(
+        title = {
+            Text(text = "Delete this log?")
+        },
+        text = {
+            Text(text = "Are you sure you want to delete ${log.title}?")
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
+
 }
