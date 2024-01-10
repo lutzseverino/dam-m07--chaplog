134f21d87bd2479012a2f71f3983bf81db5fcf12
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2024-01-10 11:03:31 +0100

    style: clean log ui state

--- app/src/main/java/cat/lasalle/chaplog/data/LogUiState.kt
+++ app/src/main/java/cat/lasalle/chaplog/data/LogUiState.kt
@@ -3,6 +3,6 @@ package cat.lasalle.chaplog.data
 data class LogUiState(
     val title: String = "",
     val author: String = "",
-    val pages: Int = 0,
     val currentPage: Int = 0,
+    val pages: Int = 0,
 )
