29231ff4441e1b018ecc0f1906e8ff23d908a347
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2024-01-10 19:30:52 +0100

    style: reformat log screen

--- app/src/main/java/cat/lasalle/chaplog/ui/LogScreen.kt
+++ app/src/main/java/cat/lasalle/chaplog/ui/LogScreen.kt
@@ -2,10 +2,7 @@ package cat.lasalle.chaplog.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,25 +18,24 @@ fun LogScreen(
     onEdit: (LogUiState) -> Unit
 ) {
     Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
-        Column (
+        Column(
             verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
             Text(text = log.title, fontWeight = FontWeight.Bold, fontSize = 20.sp)
             Text(text = "Author: ${log.author}", fontSize = 16.sp)
         }
 
-        Column (
+        Column(
             verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
             Text(text = "Current Page", fontWeight = FontWeight.Bold, fontSize = 16.sp)
             Text(text = log.currentPage.toString(), fontSize = 14.sp)
         }
 
-        Column (
+        Column(
             verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
             Text(text = "Total Pages", fontWeight = FontWeight.Bold, fontSize = 16.sp)
