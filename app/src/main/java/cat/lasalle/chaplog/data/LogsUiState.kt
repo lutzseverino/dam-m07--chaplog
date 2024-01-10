6a5831ffb95215b1c295e91f6c0958cea0b4d4df
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2024-01-10 19:32:39 +0100

    feat: add focused log to log ui state

--- app/src/main/java/cat/lasalle/chaplog/data/LogsUiState.kt
+++ app/src/main/java/cat/lasalle/chaplog/data/LogsUiState.kt
@@ -1,5 +1,6 @@
 package cat.lasalle.chaplog.data
 
 data class LogsUiState(
+    val focusedLog : LogUiState? = null,
     val logs : List<LogUiState> = emptyList()
 )
--- app/src/main/java/cat/lasalle/chaplog/model/LogsViewModel.kt
+++ app/src/main/java/cat/lasalle/chaplog/model/LogsViewModel.kt
@@ -7,25 +7,30 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.util.logging.Logger
 
 class LogsViewModel : ViewModel() {
 
     private val _uiState = MutableStateFlow(LogsUiState())
-    val uiState: StateFlow<LogsUiState> = _uiState.asStateFlow()
+    val uiState: StateFlow<LogsUiState> = _uiState
 
     fun addLog(log: LogUiState) {
+        Logger.getAnonymousLogger().info("Adding log $log")
         _uiState.update { it.copy(logs = it.logs + log) }
     }
 
     fun removeLog(log: LogUiState) {
+        Logger.getAnonymousLogger().info("Removing log $log")
         _uiState.update { it.copy(logs = it.logs - log) }
     }
 
-    fun updateLog(log: LogUiState) {
-        _uiState.update { it.copy(logs = it.logs - log + log) }
+    fun focusLog(log: LogUiState) {
+        Logger.getAnonymousLogger().info("Focusing log $log")
+        _uiState.update { it.copy(focusedLog = log) }
     }
 
     fun reset() {
+        Logger.getAnonymousLogger().info("Resetting logs")
         _uiState.update { LogsUiState() }
     }
 }
